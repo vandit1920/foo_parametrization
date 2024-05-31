ae39abb50b1f1f6ab2f67e9abda3f015a6c335e4
vandit1920 <48818526+vandit1920@users.noreply.github.com>
2024-05-31 17:47:45 -0400

    Initialised files in the testing phase Need to add directories

---
+++ test_foo_parameterization.py
@@ -0,0 +1,24 @@
+import unittest
+import math
+from foo_parameterization import FooParameterization # type: ignore
+
+class TestFooParameterization(unittest.TestCase):
+    def test_calculate_volume(self):
+        #test foo with positive given radiua
+        radius = 3
+        foo = FooParameterization(radius)
+        expected_volume = (4/3) * math.pi * (radius ** 3)
+        self.assertAlmostEqual(foo.calculate_volume(), expected_volume, places=5)
+
+    def test_negative_radius(self):
+        #test foo with ngeative given radius and raises a value error 
+        with self.assertRaises(ValueError):
+            FooParameterization(-1)
+
+    def test_zero_radius(self):
+        #test foo with a zero radius and raises a value error 
+        with self.assertRaises(ValueError):
+            FooParameterization(0)
+
+if __name__ == "__main__":
+    unittest.main()
