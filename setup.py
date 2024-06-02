e001420a6e11ec8fa744d92606863e04a7bba165
vandit1920 <48818526+vandit1920@users.noreply.github.com>
2024-06-02 18:05:46 -0400

    minute changes in the setup.py file

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='foo_parameterization',  # Package name
+    name='foo_parameterization',  
     version='0.1',
     packages=find_packages(),
     install_requires=[],
     description='Package to calculate the Foo et al. parameterization for a sphere.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Vandit Jain',
+    author_email='vanditjain19@gmail.com',
     url='https://github.com/vandit1920/foo_parametrization',
     classifiers=[
         'Programming Language :: Python :: 3',
